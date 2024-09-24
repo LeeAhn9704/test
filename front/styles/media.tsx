8e075c7d8bf5d8c4ac15cae4ffe1795eb8f3baf3
LeeAhn9704 <102342865+LeeAhn9704@users.noreply.github.com>
2024-09-24 14:10:20 +0900

    Delete front directory

--- front/styles/media.tsx
+++
@@ -1,9 +0,0 @@
-// 미디어 쿼리
-export const deviceQuery = {
-  mobile: `@media (max-width: 550px)`,
-  tablet: `@media (max-width: 850px)`,
-};
-export const mediaQuery = {
-  mobile: `(max-width: 550px)`,
-  tablet: `(max-width: 850px)`,
-};
